1ba0f4686257bd42aa319eb5828e34dfb4a43bb7
Filip Szumski <filipszumski@gmail.com>
2020-09-02 20:02:57 +0200

    Updates

---
+++ precache-manifest.5ececd571dc497daab67187530b09489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c94878c858bef61fb88a1daf6309a1",
+    "url": "/tasks-list-react/index.html"
+  },
+  {
+    "revision": "95d2b8aac89f4b3250c9",
+    "url": "/tasks-list-react/static/css/main.19b164f5.chunk.css"
+  },
+  {
+    "revision": "51382262cd4b615d1b5e",
+    "url": "/tasks-list-react/static/js/2.04831657.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasks-list-react/static/js/2.04831657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d2b8aac89f4b3250c9",
+    "url": "/tasks-list-react/static/js/main.e2e5b6fd.chunk.js"
+  },
+  {
+    "revision": "a87511fb9b67d202d797",
+    "url": "/tasks-list-react/static/js/runtime-main.03a2758e.js"
+  }
+]);
