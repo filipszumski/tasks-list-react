b34a3ca537128608229c3a838474f58582d4518b
Filip Szumski <filipszumski@gmail.com>
2020-09-20 00:09:20 +0200

    added useRef hook, and focus on input after adding new task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
+    console.log(inputRef);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        inputRef.current.focus();
     };
 
     return (
@@ -15,6 +19,7 @@ const Form = ({ addNewTask }) => {
             onSubmit={onFormSubmit}
         >
             <Input
+                ref={inputRef}
                 placeholder="Co jest do zrobienia?"
                 value={newTaskContent}
                 onChange={(event) => setNewTaskContent(event.target.value)}
